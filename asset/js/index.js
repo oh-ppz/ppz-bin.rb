67f1df68055fbc9991180fb2108cb34a1edf5b08
gy <372301467@qq.com>
2021-03-02 16:45:10 +0800

    特殊块的复制操作

--- asset/js/index.js
+++ asset/js/index.js
@@ -5,7 +5,11 @@ specialBlock()
 function specialBlock(){
   let list = document.querySelectorAll('.special-block-container')
   list.forEach(function(block){
+    let rawChildren = Array.from(block.children) // 下面添加按钮的操作，会改变 children 对象，但变成数组后，就不会了
     let copyBtn = document.createElement('button')
+    copyBtn.onclick = function(){
+      navigator.clipboard.writeText(rawChildren.map(child => child.innerHTML).join('\n'))
+    }
     copyBtn.classList.add('copy')
     block.appendChild(copyBtn)
   })
